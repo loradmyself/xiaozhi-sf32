eff1fecfc80766612dfb47abf06b67836f16af02
SiFli <sifli@xiaozhi.com>
2025-09-09 17:41:31 +0800

    BLUETOOTH_NAME

--- app/src/main.c
+++ app/src/main.c
@@ -779,7 +779,7 @@ int main(void)
                     addr.addr[0], addr.addr[1], addr.addr[2],
                     addr.addr[3], addr.addr[4], addr.addr[5]);
 #else
-            const char *local_name = BLUETOOTH_NAME;
+            const char *local_name = "BLUETOOTH_NAME";
 #endif
             bt_interface_set_local_name(strlen(local_name), (void *)local_name);
         }
