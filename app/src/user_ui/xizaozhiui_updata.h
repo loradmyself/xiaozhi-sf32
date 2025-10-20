05a9126a69164ba49bcf4c9785f334c406aa2f8c
loadmyself <2423698115@qq.com>
2025-10-20 12:54:39 +0800

    standby_ui screen in xiazhi_update.c

--- app/src/user_ui/xizaozhiui_updata.h
+++ app/src/user_ui/xizaozhiui_updata.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "./../xiaozhi_weather.h"
+#include "gui_guider.h"
+extern lv_ui standby_screen;
 extern date_time_t g_current_time ;
 //time_ui_update_callback
 #define TIME_GET xiaozhi_time_get_current(&g_current_time)
